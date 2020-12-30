6bd505e93f21f8a2a799b9fd312b49db86667170
Simon Lee <simon@leefamily.uk.net>
2020-12-30 23:41:27 +0000

    Update setup_panel.sh
    Updated mysql_secure_installation - Line 73

--- current/ubuntu/setup_panel.sh
+++ current/ubuntu/setup_panel.sh
@@ -70,7 +70,7 @@ SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
 expect \"Enter current password for root (enter for none):\"
-send \"$MYSQL\r\"
+send \"y\r\"
 expect \"Change the root password?\"
 send \"n\r\"
 expect \"Remove anonymous users?\"
